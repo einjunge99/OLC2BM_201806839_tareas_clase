a526ad7851c31d7b76665b59f724e5de22e886f2
einjunge99 <57040720+einjunge99@users.noreply.github.com>
2020-08-31 08:37:21 -0700

    Actualización main.js

--- OLC2BM_201114595_tareas_clase/Tarea11/main.js
+++ OLC2BM_201114595_tareas_clase/Tarea11/main.js
@@ -1,12 +1,11 @@
 var parser = require('./Gramatica/gramatica');
-const readline = require('readline');
 
-let ast;
+let salida;
 try {
     //---donde entrada representa una cadena aritmética
     const entrada ="1+3/4*2"
-    ast = parser.parse(entrada.toString());
-    console.log(ast)
+    salida = parser.parse(entrada.toString());
+    console.log(salida)
 } catch (e) {
     console.error(e);
     return;
